fa475bc4d52133ec7c5a803b301bd9fd37f5dc6b
U-DESKTOP-CI17OKR\Jacek <pketk@op.pl>
2023-09-29 00:38:02 +0200

    Mask implementation - being stupid is tiring...

--- main.cpp
+++ main.cpp
@@ -3,12 +3,14 @@
 #include <iostream>
 #include <regex>
 
+#define NUM_MASK IP.substr(IP.find_first_of('/', 0) + 1, IP.length() - IP.find_first_of('/', 0))
+
 int main(int argc, char **argv) {
     // argv[0] is directory
     // argv[1] is the input IP address
 
     // IP validation regex
-    std::regex ipv4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])");
+    std::regex ipv4("(([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])\\.){3}([0-9]|[1-9][0-9]|1[0-9][0-9]|2[0-4][0-9]|25[0-5])(?:/[1-2]\\d|/3[0-1]|/[1-9])?");
     std::string IP = argv[1];
 
     //  no arguments
@@ -27,9 +29,17 @@ int main(int argc, char **argv) {
         std::cout << "Invalid IP address!" << std::endl;
     }
 
-    //  TODO: all ipcalc functionalities, text formatting (cout formatting), possibility of checking the mask, etc.
-    else {
+    //  TODO: fix cout formatting, fix wildcard function (reverseOctet) functionalities: network, broadcast, host min, host max, hosts/net
+
+    //  without providing network mask
+    else if (IP.find_first_of('/', 0) == IP.npos) {
         std::cout << std::right << "Address: " + IP << std::setw(13) << " " << std::setw(1) << Functions::ipToBits(IP) << std::endl;
+    }
+
+    else {
+        std::cout << std::right << "Address: " + IP.substr(0, IP.find_first_of('/', 0)) << std::setw(13) << " " << std::setw(1) << Functions::ipToBits(IP) << std::endl;
+        std::cout << std::right << "Netmask: " + Functions::getNetmask(std::stoi(NUM_MASK)) + " = " + NUM_MASK << std::setw(13) << " " << std::setw(1) << Functions::ipToBits(Functions::getNetmask(std::stoi(NUM_MASK))) << std::endl;
+        std::cout << std::right << "Wildcard: " + Functions::getWildcard(std::stoi(NUM_MASK)) << std::setw(13) << " " << std::setw(1) << Functions::ipToBits(Functions::getWildcard(std::stoi(NUM_MASK))) << std::endl;
     }
 
     return 0;
